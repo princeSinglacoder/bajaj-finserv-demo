2b7b4ffd05ccc6d818c139fdb0285b2afb355211
Prince <princesingla8570@gmail.com>
2025-07-29 21:04:15 +0530

    Initial commit: Bajaj Finserv Spring Boot demo project

---
+++ src/main/java/com/bajaj/bajaj_finserv_demo/BajajFinservDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bajaj.bajaj_finserv_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajFinservDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajajFinservDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajaj/bajaj_finserv_demo/BajajFinservDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaj.bajaj_finserv_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajFinservDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
