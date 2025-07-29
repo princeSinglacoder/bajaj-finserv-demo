43698cd975cfdf9d16e2c78405ab0de00a7ea741
Prince Singla <singlaprince349@gmail.com>
2025-07-29 21:30:53 +0530

    Completed controller class

--- src/main/java/com/bajaj/bajaj_finserv_demo/controller/BajajController.java
+++ src/main/java/com/bajaj/bajaj_finserv_demo/controller/BajajController.java
@@ -2,6 +2,8 @@ package com.bajaj.bajaj_finserv_demo.controller;
 
 import com.bajaj.bajaj_finserv_demo.controller.model.BajajRequest;
 import com.bajaj.bajaj_finserv_demo.controller.model.BajajResponse;
+import com.bajaj.bajaj_finserv_demo.service.BajajService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bfhl")
 public class BajajController {
 
+    @Autowired
+    private BajajService bajajService;
+
     public BajajResponse processData(@RequestBody BajajRequest request){
-        return null;
+        return bajajService.processData(request);
     }
 }
--- src/main/java/com/bajaj/bajaj_finserv_demo/service/BajajService.java
+++ src/main/java/com/bajaj/bajaj_finserv_demo/service/BajajService.java
@@ -1,4 +1,11 @@
 package com.bajaj.bajaj_finserv_demo.service;
 
+import com.bajaj.bajaj_finserv_demo.controller.model.BajajRequest;
+import com.bajaj.bajaj_finserv_demo.controller.model.BajajResponse;
+
 public class BajajService {
+
+    public BajajResponse processData(BajajRequest request) {
+        return null;
+    }
 }
