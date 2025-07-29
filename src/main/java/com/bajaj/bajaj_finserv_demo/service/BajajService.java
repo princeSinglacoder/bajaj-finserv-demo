d8e325e16d49bab575a5a4a5dc17487127e658a6
Prince Singla <singlaprince349@gmail.com>
2025-07-29 21:59:05 +0530

    Added annotation

--- src/main/java/com/bajaj/bajaj_finserv_demo/service/BajajService.java
+++ src/main/java/com/bajaj/bajaj_finserv_demo/service/BajajService.java
@@ -2,10 +2,12 @@ package com.bajaj.bajaj_finserv_demo.service;
 
 import com.bajaj.bajaj_finserv_demo.controller.model.BajajRequest;
 import com.bajaj.bajaj_finserv_demo.controller.model.BajajResponse;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class BajajService {
 
     public BajajResponse processData(BajajRequest bajajRequest) {
